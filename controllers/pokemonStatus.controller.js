618bebb3b301cc78f20c9e3e97e134f8f64eff86
J-Garzon18 <alejandro.garzon18164@gmail.com>
2025-05-12 12:09:01 -0500

    creacion de change Pokemon

--- controllers/pokemonStatus.controller.js
+++ controllers/pokemonStatus.controller.js
@@ -13,4 +13,39 @@ const createPokemon= async(req,res) => {
     }
 }
 
-export default createPokemon
+const getpokemons = async(req,res)=>{
+    try{
+        const pokemons=await pokemonStatusModel.find()
+        return res.status(200).json(pokemons)
+    } catch { 
+        return res.status(500)
+        .json({"code":500,"message":error.message,"status":false})
+    }
+}
+
+const changeStatePokemon = async (req,res)=> {
+    try {
+    const pokemon_id = req.params.pokemon.pokemon_id
+    if(req.params.pokemon_id == req.body.pokemon_id){
+        const changePokemon = {
+        pokemon_id:pokemon_id,
+        view:req.body.view,
+        catch:req.body.catch,
+        in_team:req.body.in_team,
+        power_level:req.body.level
+        }
+         let filter= {pokemon_id:pokemon_id}
+    const pokemon = await pokemonStatusModel
+    .findOneAndReplace(filter,changePokemon,{new:true})
+    return res.status(200).json(pokemon)  
+    } else {
+        return res.status(400).json(({"code":500,"message":"Bad request : id diferente en el body","status":false}))
+    }
+    }catch (error){
+        return res.status(500)
+        .json({"code":500,"message":error.message,"status":false})
+    }
+
+}
+
+export default {createPokemon,getpokemons,changeStatePokemon}
--- routes/pokemonStatus.routes.js
+++ routes/pokemonStatus.routes.js
@@ -1,8 +1,11 @@
 import express from "express"
 import createPokemon from "../controllers/pokemonStatus.controller.js"
+import pokemonControllers from "../controllers/pokemonStatus.controller.js"
 
 const router = express.Router()
 
-router.post("/",createPokemon)
+router.post("/",pokemonControllers.createPokemon)
+router.get("/",pokemonControllers.getpokemons)
+router.put("/pokemon_id",pokemonControllers.changeStatePokemon)
 
 export default router
